30b2750ba0a5790fcca6627b26fbe495e4910f08
SleepyDude <ko_on@mail.ru>
2023-01-02 20:15:56 +0800

    change application, now its poor random thing

--- app/api/fake_data.py
+++ app/api/fake_data.py
@@ -1,26 +1,10 @@
 from fastapi import APIRouter, Depends, Request, HTTPException
 from pydantic import ValidationError
-from fastapi.exceptions import RequestValidationError
 from faker import Faker
-from typing import Optional, Union, Tuple
-from dataclasses import dataclass
-
-from app import constants as cons
-from ..models.fake_data import QueryParams, VALID_SHAPES
-from ..models.utils import make_dependable
+from typing import Optional
+import random
 
 router = APIRouter()
-
-'''static scheme
- id,
- name,
- address,
- email,
- phone,
- birthday,
-'''
-
-MAX_ITEMS = 300
 
 async def gen_row(fake: Faker) -> dict:
     return {
@@ -32,115 +16,25 @@ async def gen_row(fake: Faker) -> dict:
         'birthday': fake.date()
     }
 
-# async def gen_row(fake: Faker) -> dict:
-#     return {
-#         'id': fake.unique.random_number()
-#     }
-
-class DispenserBase:
-    def __init__(self):
-        ...
-
-    async def generate_data(self):
-        raise NotImplementedError('You should implement `generate_data` method in child')
-
-class DispenserRNG(DispenserBase):
-    '''
-    Generates random data of given size
-    '''
-    def __init__(self, size):
-        self.size = size
-    
-    async def generate_data(self):
-        fake = Faker()
-        return [await gen_row(fake) for _ in range(self.size)]
-
-async def query_data_validate(size, page, begin, end, seed) -> Tuple[bool, str]:
-    # create combination shape for the given params
-    params = [size, page, begin, end]
-    comb = [1 if i is not None else 0 for i in params]
-
-    print('get the comb:', comb)
-    # check if comb is valid
-    if comb not in VALID_SHAPES:
-        return False, cons.WRONG_COMB.format(comb)
-    
-    if size is not None:
-        ...
-
-    return True, 'Success params'
-
-
-# async def get_despenser(size, page, begin, end, seed)\
-#         -> Optional[Union[DispenserRNG, None]]:
-async def get_despenser(q: QueryParams)\
-        -> Optional[Union[DispenserRNG, None]]:
-    params = [q.size, q.page, q.begin, q.end]
-    comb = [1 if i is not None else 0 for i in params]
-    
-    # let's take care about cases with RNG
-    if q.seed is None:
-        if comb == VALID_SHAPES[5] or\
-           comb == VALID_SHAPES[6]:
-            return DispenserRNG(20)
-        if comb == VALID_SHAPES[0] or\
-           comb == VALID_SHAPES[1] or\
-           comb == VALID_SHAPES[2] or\
-           comb == VALID_SHAPES[3]:
-            return DispenserRNG(q.size)
-        if comb == VALID_SHAPES[4]:
-            return DispenserRNG(q.end-q.begin)
-    # it's harder if it's seeded
-    raise Exception('Seeds not working for now')
-    if comb == VALID_SHAPES[0]:
-        ...
-
 @router.get('/fake_users/')
-async def fake_users(query_params: QueryParams = Depends(make_dependable(QueryParams))):
-    '''Get random user data --> {'detail' : [{'msg'},] , 'data' : [...]}
-
-        Supported the following query parameters:
-
-        `size`: int -- rows of data
-        `seed`: int -- RNG seed
-        `page`: int -- page number
-        `begin`: int -- first element index
-        `end`: int -- last element index
-
-        Valid combinations:
-            Pages -- standart pagination interface
-            - `size` - data per page
-            - `page` - page number
-
-            Interval -- getting elements from a given half-open interval `[begin, end)`
-            - `begin` - start fake element index
-            - `end` - next to finish element index
-
-            LIMITATION -- both `begin` and `end` should be in `[0; 201)` half-open interval
-            
-            Bottom-up -- getting elements from a half-open interval `[begin, begin+size)`
-            - `begin` - start fake element index
-            - `size` - number of items from element with specific index
-
-            LIMITATION -- `begin` and `size` should be in `[0; 201)` half-open interval
-
-            Top-down -- getting elements from a half-open interval `(end-size-1, end]`
-            - `end` - finish element index
-            - `size` - number of items from element with specific index
-
-            LIMITATION -- `end` and `size` should be in [0; 201) half-open interval
+async def fake_users(request: Request):
+    '''Get random user data based on query parameters if any
+    --> {'detail' : [{'msg'},] , 'data' : [...]}
+    You will get [1, 50) rows of fake user data
     '''
-    # print(f'got {query_params} params in /fake_users endpoint')
-    # return {'message': f'got {query_params} params in /fake_users endpoint'}
-    msg = f'Got query params: {query_params}'
-    desp = await get_despenser(query_params)
-    if desp is not None:
-        data = await desp.generate_data()
+    params = request.query_params
+    params_string = str(params)
+    size = random.randint(1, 50)
+
+    faker = Faker() 
+    if params is not None:
+        Faker.seed(params_string)
+
+    data = [await gen_row(faker) for _ in range(size)]
     return {
         'detail': {
-            'status': 'success',
             'data-length': len(data),
-            'msg' : msg
+            'msg' : 'success'
         },
         'data': data
     }
--- app/app.py
+++ app/app.py
@@ -1,19 +1,7 @@
 from fastapi import FastAPI
 
-from .api import root
-from .api import users
 from .api import fake_data
 
 app = FastAPI()
 
-app.include_router(root.router)
-app.include_router(users.router)
 app.include_router(fake_data.router)
-
-# @app.on_event("startup")
-# async def startup_event():
-#     await init_deta()
-
-# @app.on_event("shutdown")
-# async def shutdown_event():
-#     await shutdown_deta()
